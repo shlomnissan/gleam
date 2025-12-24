353241888a4e42a8da16495c9d9f9bd1795c8f13
Shlomi Nissan <admin@betamark.com>
2025-12-24 05:33:29 -0800

    Add return values to file helpers

--- src/utilities/file.hpp
+++ src/utilities/file.hpp
@@ -10,15 +10,18 @@
 namespace vglx {
 
 template <typename T>
-std::enable_if_t<std::is_trivially_copyable_v<T>>
+std::enable_if_t<std::is_trivially_copyable_v<T>, bool>
 read_binary(std::istream& in, T& value) {
     in.read(reinterpret_cast<char*>(&value), sizeof(T));
+    return in.gcount() == static_cast<std::streamsize>(sizeof(T));
 }
 
 template <typename T>
-std::enable_if_t<std::is_trivially_copyable_v<T>>
+std::enable_if_t<std::is_trivially_copyable_v<T>, bool>
 read_binary(std::istream& in, std::vector<T>& vec, std::size_t count) {
-    in.read(reinterpret_cast<char*>(vec.data()), count);
+    vec.resize(count);
+    in.read(reinterpret_cast<char*>(vec.data()), count * sizeof(T));
+    return in.gcount() == static_cast<std::streamsize>(count * sizeof(T));
 }
 
 }
