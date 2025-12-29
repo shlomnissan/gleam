85332a1c7bab967eca61e2bd842eb656a4448cc6
Shlomi Nissan <admin@betamark.com>
2025-12-29 06:19:47 -0800

    Update mesh and texture loader tests

--- tests/loaders/texture_loader_test.cpp
+++ tests/loaders/texture_loader_test.cpp
@@ -6,75 +6,32 @@
 */
 
 #include <gtest/gtest.h>
+#include <print>
 
-#include <vglx/loaders/texture_loader.hpp>
+#include <vglx/textures/texture_2d.hpp>
 
-#include <future>
-#include <thread>
+#include "loaders/texture_loader_xyz.hpp"
 
-const auto texture_loader = vglx::TextureLoader::Create();
+TEST(TextureLoader, LoadTexture) {
+    auto result = vglx::load_texture("assets/texture.tex");
+    EXPECT_TRUE(result.has_value());
 
-#pragma region Helpers
-
-template <typename Callback>
-auto RunAsyncTest(const std::string& file_path, Callback callback) {
-    auto main_thread_id = std::this_thread::get_id();
-    auto promise = std::promise<void> {};
-    auto future = promise.get_future();
-
-    texture_loader->LoadAsync(file_path, [&](const auto& result) {
-        callback(result, main_thread_id);
-        promise.set_value();
-    });
-
-    auto status = future.wait_for(std::chrono::seconds(1));
-    EXPECT_EQ(status, std::future_status::ready);
-}
-
-auto VerifyImage(const auto& texture, const std::string& filename) {
+    auto texture = result.value();
     EXPECT_EQ(texture->data.size(), 5 * 5 * 4);
     EXPECT_EQ(texture->width, 5);
     EXPECT_EQ(texture->height, 5);
 }
 
-#pragma endregion
+TEST(TextureLoader, LoadTextureInvalidFileFormat) {
+    auto result = vglx::load_texture("assets/texture.png");
 
-#pragma region Load Image Synchronously
-
-TEST(TextureLoader, LoadTextureSynchronous) {
-    auto result = texture_loader->Load("assets/texture.tex");
-    VerifyImage(result.value(), "texture.tex");
-}
-
-TEST(TextureLoader, LoadTextureSynchronousInvalidFileType) {
-    auto result = texture_loader->Load("assets/texture.png");
-    EXPECT_FALSE(result);
+    EXPECT_EQ(false, result.has_value());
     EXPECT_EQ(result.error(), "Invalid texture file 'assets/texture.png'");
 }
 
-TEST(TextureLoader, LoadTextureSynchronousInvalidFile) {
-    auto result = texture_loader->Load("assets/invalid_texture.tex");
-    EXPECT_FALSE(result);
-    EXPECT_EQ(result.error(), "File not found 'assets/invalid_texture.tex'");
+TEST(TextureLoader, LoadTextureInvalidPath) {
+    auto result = vglx::load_texture("assets/invalid_path.tex");
+
+    EXPECT_EQ(false, result.has_value());
+    EXPECT_EQ(result.error(), "Unable to open texture 'assets/invalid_path.tex'");
 }
-
-#pragma endregion
-
-#pragma region Load Image Asynchronously
-
-TEST(TextureLoader, LoadTextureAsynchronous) {
-    RunAsyncTest("assets/texture.tex", [](const auto& result, const auto& main_thread_id) {
-        VerifyImage(result.value(), "texture.tex");
-        EXPECT_NE(main_thread_id, std::this_thread::get_id());
-    });
-}
-
-TEST(TextureLoader, LoadTextureAsynchronousInvalidFileType) {
-    RunAsyncTest("assets/texture.png", [](const auto& result, const auto& main_thread_id) {
-        EXPECT_FALSE(result);
-        EXPECT_EQ(result.error(), "Invalid texture file 'assets/texture.png'");
-        EXPECT_NE(main_thread_id, std::this_thread::get_id());
-    });
-}
-
-#pragma endregion
