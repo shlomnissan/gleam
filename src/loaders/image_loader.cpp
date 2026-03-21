1722f98d6bb3ffe78f20a898727c071ff3ba9e67
Shlomi Nissan <admin@betamark.com>
2026-03-21 14:02:02 -0700

    Add file ext validation to image loader

--- src/loaders/image_loader.cpp
+++ src/loaders/image_loader.cpp
@@ -11,14 +11,33 @@
 #include "utilities/assert.hpp"
 #include "utilities/logger.hpp"
 
+#include <algorithm>
+#include <array>
 #include <memory>
 
 namespace vglx {
+
+namespace {
+
+std::array<std::string, 6> supported_file_ext {
+    ".png", ".jpg", ".jpeg", ".tga", ".bmp", ".hdr"
+};
+
+auto is_supported_file_ext(const fs::path& path) {
+    return std::ranges::find(
+        supported_file_ext, path.extension().string()
+    ) != supported_file_ext.end();
+}
+
+}
 
 ImageLoader::ImageLoader(LoadScheduler* scheduler) : load_scheduler_(scheduler) {}
 
 
 auto ImageLoader::Load(const fs::path& path) const -> std::expected<Image, std::string> {
+    if (!is_supported_file_ext(path)) {
+        return std::unexpected("Unsupported file extension");
+    }
     return detail::image::import(path);
 }
 
@@ -27,6 +46,12 @@ auto ImageLoader::LoadAsync(const fs::path& path) const -> ImageLoadHandle {
 
     auto state = std::make_shared<ImageLoadHandle::State>();
     auto handle = ImageLoadHandle {state};
+
+    if (!is_supported_file_ext(path)) {
+        state->error = "Unsupported file extension";
+        state->ready = true;
+        return handle;
+    }
 
     load_scheduler_->Enqueue(
         [state, path] {
