963dc7354c8f7bb6425f803ebba6cfdb46e3eea3
Shlomi Nissan <admin@betamark.com>
2026-05-25 06:04:12 -0700

    Add tone mapping example

---
+++ examples/scenes/tone_mapping.cpp
@@ -0,0 +1,76 @@
+/*
+===========================================================================
+  VGLX https://vglx.org
+  Copyright © 2024 - Present, Shlomi Nissan
+===========================================================================
+*/
+
+#include "example_runner.hpp"
+
+auto GetCamera() {
+    auto camera = vglx::PerspectiveCamera::Create({
+        .fov = vglx::math::DegToRad(60.0f),
+        .aspect = static_cast<float>(kWindowWidth) / static_cast<float>(kWindowHeight),
+        .near = 0.3f,
+        .far = 1000.0f
+    });
+
+    camera->transform.SetPosition({0.0f, 1.0f, 5.0f});
+
+    return camera;
+}
+
+auto GetScene() {
+    auto scene = vglx::Scene::Create();
+
+    auto sphere = vglx::SphereGeometry::Create({
+        .width_segments = 64,
+        .height_segments = 32
+    });
+
+    auto sphere_material = vglx::PBRMaterial::Create(0xB3B3B3);
+    sphere_material->metallic = 1.0f;
+    sphere_material->roughness = 0.2f;
+
+    scene->Add(vglx::Mesh::Create(sphere, sphere_material));
+
+    auto plane = vglx::PlaneGeometry::Create({
+        .width = 20.0f,
+        .height = 20.0f
+    });
+
+    auto plane_material = vglx::PBRMaterial::Create(0x808080);
+    plane_material->metallic = 0.0f;
+    plane_material->roughness = 0.8f;
+
+    scene->Add(
+        vglx::Mesh::Create(plane, plane_material)
+    )->RotateX(vglx::math::DegToRad(-90.0f));
+
+    scene->Add(vglx::AmbientLight::Create({
+        .color = 0xFFFFFF,
+        .intensity = .05f
+    }));
+
+    scene->Add(vglx::PointLight::Create({
+        .color = 0xFFFFFF,
+        .intensity = 50.0f,
+        .attenuation = {
+            .base = 1.0f,
+            .linear = 0.0f,
+            .quadratic = 1.0f
+        }
+    }))->transform.Translate({1.5f, 3.0f, 1.5f});
+
+    return scene;
+}
+
+auto main() -> int {
+    auto camera = GetCamera();
+    auto scene = GetScene();
+
+    return RunExample(scene.get(), camera.get(), {
+        .window_title = "Tone Mapping",
+        .clear_color = 0x000000
+    });
+}
